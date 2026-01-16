6feaf21a4f9f326f242cb8c87891ef97515b93c2
Shaedyr <jonas95martensson@gmail.com>
2026-01-16 14:49:20 +0100

    Create cell_mapping.py

---
+++ app_modules/cell_mapping.py
@@ -0,0 +1,27 @@
+CELL_MAP = {
+
+  # From (Brønnøysund,Proff SHEET 1)
+  
+    "Sammendrag": {
+        # B2 intentionally skipped (Ansvarlig megler)
+
+        "company_name": "B3",          # Kunde
+        "org_number": "B4",            # Orgnr
+        "address": "B5",               # Adresse
+        "post_nr": "B6",               # Postnr
+        "city": "B7",                  # Poststed
+        "employees": "B8",             # Antall ansatte
+        "nace_code": "B9",             # Nace-kode
+        "nace_description": "B10",     # NACE-bransje
+        "homepage": "B11",             # Hjemmeside
+        "claims_history": "B12",       # Skadetall
+        "company_summary": "B13",      # Om bedriften
+        "current_insurer": "B14",      # Dagens selskap
+        "tender_deadline": "B15",      # Anbudsfrist
+        "revenue_2024": "B16",         # Omsetning 2024 (Proff.NO)
+        "financials": "B17",           # Økonomi
+    }
+  
+  # More sheets will be added here
+
+}
