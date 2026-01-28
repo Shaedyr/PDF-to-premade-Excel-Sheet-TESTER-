65b16e6dbe08762884f30b5fc93d87ee3eb07387
Shaedyr <jonas95martensson@gmail.com>
2026-01-28 09:41:25 +0100

    Update __init__.py

--- app_modules/Sheets/Fordon/extractors/__init__.py
+++ app_modules/Sheets/Fordon/extractors/__init__.py
@@ -1,12 +1,11 @@
 # app_modules/Sheets/Fordon/extractors/__init__.py
 """
 Vehicle extractors for different insurance companies.
+Each company has its own extractor file.
 """
 
-from .if_skadeforsikring import extract_if_vehicles
-from .gjensidige import extract_gjensidige_vehicles
-
+# Don't import here - let mapping.py import directly from the modules
 __all__ = [
-    'extract_if_vehicles',
-    'extract_gjensidige_vehicles',
+    'if_skadeforsikring',
+    'gjensidige',
 ]
