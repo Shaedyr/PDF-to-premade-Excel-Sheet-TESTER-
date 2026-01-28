7d014f371d38dedfc31594572abc6cd802bb68bf
Shaedyr <jonas95martensson@gmail.com>
2026-01-28 09:39:08 +0100

    Update __init__.py

--- app_modules/Sheets/Fordon/extractors/__init__.py
+++ app_modules/Sheets/Fordon/extractors/__init__.py
@@ -1,15 +1,10 @@
 # app_modules/Sheets/Fordon/extractors/__init__.py
 """
 Vehicle extractors for different insurance companies.
-
-To add a new company:
-1. Create new file: company_name_format.py
-2. Add import here
-3. Add to __all__
 """
 
-from .if_format import extract_if_vehicles
-from .gjensidige_format import extract_gjensidige_vehicles
+from .if_skadeforsikring import extract_if_vehicles
+from .gjensidige import extract_gjensidige_vehicles
 
 __all__ = [
     'extract_if_vehicles',
